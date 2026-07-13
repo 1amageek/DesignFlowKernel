a62b2f52e8f88eab6282c0e4a9ca174c094e80d3
1amageek <tmy0x3@icloud.com>
2026-07-13 23:06:24 +0900

    Use Foundation artifact selectors in flow toolchain records

--- Sources/DesignFlowKernel/FlowToolchainStageArtifactSelectorRecord.swift
+++ Sources/DesignFlowKernel/FlowToolchainStageArtifactSelectorRecord.swift
@@ -1,17 +1,18 @@
+import CircuiteFoundation
 import Foundation
 
 public struct FlowToolchainStageArtifactSelectorRecord: Sendable, Hashable, Codable {
     public var stageID: String
     public var artifactID: String?
-    public var kind: XcircuiteFileKind?
-    public var format: XcircuiteFileFormat?
+    public var kind: ArtifactKind?
+    public var format: ArtifactFormat?
     public var pathSuffix: String?
 
     public init(
         stageID: String,
         artifactID: String? = nil,
-        kind: XcircuiteFileKind? = nil,
-        format: XcircuiteFileFormat? = nil,
+        kind: ArtifactKind? = nil,
+        format: ArtifactFormat? = nil,
         pathSuffix: String? = nil
     ) {
         self.stageID = stageID
